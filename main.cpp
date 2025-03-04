df76fca86aab13c58e4c2b650647ced660d9a43b
Daniel Park <intheimageofyeshua@gmail.com>
2025-03-04 10:23:04 +0900

    Applied Try Catch Block

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <SDL2/SDL_timer.h>
 #define FMT_HEADER_ONLY
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_error.h>
 #include <SDL2/SDL_render.h>
+#include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <fmt/format.h>
 #include <iostream>
@@ -15,41 +15,50 @@ constexpr int WINDOW_WIDTH = 800;
 constexpr int WINDOW_HEIGHT = 600;
 
 int main(void) {
-    SDL_Window *window{nullptr};
-    SDL_Renderer *renderer{nullptr};
+    try {
+        SDL_Window *window{nullptr};
+        SDL_Renderer *renderer{nullptr};
 
-    if (SDL_Init(SDL_FLAGS) != 0) {
-        auto error = fmt::format("Error initializing SDL2: {}", SDL_GetError());
-        std::cerr << error << '\n';
+        if (SDL_Init(SDL_FLAGS) != 0) {
+            auto error =
+                fmt::format("Error initializing SDL2: {}", SDL_GetError());
+            throw std::runtime_error(error);
+        }
+
+        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,
+                                  SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
+                                  WINDOW_HEIGHT, 0);
+
+        if (!window) {
+            auto error =
+                fmt::format("Error creating Window: {}", SDL_GetError());
+            throw std::runtime_error(error);
+        }
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+        if (!renderer) {
+            auto error =
+                fmt::format("Error creating Renderer: {}", SDL_GetError());
+            throw std::runtime_error(error);
+        }
+
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+
+        SDL_Delay(5000);
+
+        SDL_DestroyRenderer(renderer);
+        renderer = nullptr;
+
+        SDL_DestroyWindow(window);
+        window = nullptr;
+
+        SDL_Quit();
+    } catch (const std::runtime_error &e) {
+        std::cerr << e.what() << "\n";
+        return 1;
     }
-
-    window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH,
-                              WINDOW_HEIGHT, 0);
-
-    if (!window) {
-        auto error = fmt::format("Error creating Window: {}", SDL_GetError());
-        std::cerr << error << '\n';
-    }
-
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (!renderer) {
-        auto error = fmt::format("Error creating Renderer: {}", SDL_GetError());
-        std::cerr << error << '\n';
-    }
-
-    SDL_RenderClear(renderer);
-
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(5000);
-
-    SDL_DestroyRenderer(renderer);
-    renderer = nullptr;
-
-    SDL_DestroyWindow(window);
-    window = nullptr;
-
-    SDL_Quit();
+    return 0;
 }
